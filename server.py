6689fe49788e420502d1c0a38825a1fe234f1e83
CH4C2H6 <CH4C2H6@GMAIL.COM>
2020-09-05 18:29:02 +0200

    Frontend update

--- server.py
+++ server.py
@@ -1,19 +1,30 @@
 import http.server
 import socketserver
 import re
+from icmplib import ping, multiping, traceroute, Host, Hop
 
 PORT = 9090
 
+
 class CustomHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        if None != re.search('/api/square/*', self.path):
-            num = float(self.path.split('/')[-1])
-            print (self.path.split('/'))
+        if None != re.search('/api/ping/*', self.path):
+            ipaddr = ""+self.path.split('/')[-1]
+            print(self.path.split('/'))
             #This URL will trigger our sample function and send what it returns back to the browser
             self.send_response(200)
             self.send_header('Content-type','text/html')
             self.end_headers()
-            self.wfile.write(str(num*num).encode()) #call sample function here
+            self.wfile.write( str(ping(ipaddr, count=3, interval=0.2, timeout=1).avg_rtt).encode() ) #call sample function here
+            return 
+        if None != re.search('/api/isalive/*', self.path):
+            ipaddr = ""+self.path.split('/')[-1]
+            print(self.path.split('/'))
+            #This URL will trigger our sample function and send what it returns back to the browser
+            self.send_response(200)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+            self.wfile.write( str(ping(ipaddr, count=3, interval=0.2, timeout=1).is_alive).encode() ) #call sample function here
             return
         if None != re.search('/api/mult/*', self.path):
             num1 = float(self.path.split('/')[-1])
